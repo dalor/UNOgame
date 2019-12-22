0ce319c57ceff3e1ab10ff7421c0433d8a4d2933
hitrch <vladislawpaliy@gmail.com>
2019-12-22 16:57:29 +0200

    websocket file added

---
+++ frontend/src/services/websocket/websocket.js
@@ -0,0 +1,27 @@
+let connection = new WebSocket('ws://127.0.0.1:1337');
+
+connection.onopen = function () {
+};
+
+connection.onerror = function (error) {
+};
+
+/*connection.onmessage = function (message) {
+    try {
+        let json = JSON.parse(message.data);
+        switch (json.type) {
+                case "SET_USER":{
+                    //console.log(json);
+                    break;
+                }
+                default: {
+                    console.log('No type message');
+                    break;
+                }
+        }
+    } catch (e) {
+        console.log(e);
+    }
+};*/
+
+export default connection;
